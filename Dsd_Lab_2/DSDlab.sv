b768742c86d66b98fb6681c37a4f6e8fea4a28d3
Danish-Hassann <danishhasan095@gmail.com>
2025-02-03 23:55:57 +0500

    Update DSDlab.sv

--- Dsd_Lab_2/DSDlab.sv
+++ Dsd_Lab_2/DSDlab.sv
@@ -22,6 +22,6 @@
 
 module DSDlab3( output x,y,
     input a,b,c);
-    assign x = !c ^ (a & b);
-    assign y = (!(a & b) ^ (a | b)) & (a & b); 
+    assign x = ~c ^ (a & b);
+    assign y = (~(a & b) ^ (a | b)) & (a & b); 
 endmodule
